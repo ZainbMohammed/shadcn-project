5e610cce70111ee5f632fef53afb551d7ea7fba6
Zainab <pk.x3311@gmail.com>
2024-09-23 23:27:40 +0300

    create loading page to display SkeletonCard when loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ interface Recipe {
 async function getRecipes(): Promise<Recipe[]> {
   const result = await fetch("http://localhost:4000/recipes");
 
+  await new Promise((resolve) => setTimeout(resolve, 500));
   return result.json();
 }
 
